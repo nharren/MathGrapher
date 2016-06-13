70bcd97e01f5129707b579c2ba9870a74532f0df
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2016-06-12 20:13:04 -0700

    Add initial files.

---
+++ MathGrapher/AxisDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace MathGrapher
+{
+    public class AxisDefinition
+    {
+        public Color Color { get; set; }
+        public double Interval { get; set; }
+        public double Max { get; set; }
+        public double Min { get; set; }
+        public double Thickness { get; set; }
+    }
+}
---
+++ MathGrapher/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("MathGrapher")]
+[assembly: AssemblyDescription("Graphs 2D functions")]
+[assembly: AssemblyProduct("MathGrapher")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("1.0.*")]
